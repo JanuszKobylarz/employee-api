f6c87dd9cb6122afda763ce13326af8257df4691
Janusz <janusz.kobylarz@growcode.com>
2024-06-11 14:07:43 +0200

    Initial migration-

--- migrations/Version20240607173609.php
+++ migrations/Version20240607173609.php
@@ -22,6 +22,7 @@ final class Version20240607173609 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE employee (id INT AUTO_INCREMENT NOT NULL, parent_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, surname VARCHAR(255) NOT NULL, position VARCHAR(255) NOT NULL, INDEX IDX_5D9F75A1727ACA70 (parent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE employee ADD CONSTRAINT FK_5D9F75A1727ACA70 FOREIGN KEY (parent_id) REFERENCES employee (id)');
+        $this->addSql('INSERT INTO employee (id, parent_id, name, surname, position) VALUES (1, NULL, \'Jan\', \'Kowalski\', \'Prezes\')');
     }
 
     public function down(Schema $schema): void
